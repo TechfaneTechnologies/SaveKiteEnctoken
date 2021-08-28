65b0f2e7b9a3c0ae209f602c3518651474efdf49
Shabbir Hasan <68828793+ShabbirHasan1@users.noreply.github.com>
2021-08-28 12:32:15 +0530

    Update SaveKiteEnctoken.user.js

--- SaveKiteEnctoken.user.js
+++ SaveKiteEnctoken.user.js
@@ -2,7 +2,7 @@
 // @name         SaveKiteEnctoken
 // @namespace    https://github.com/TechfaneTechnologies/SaveKiteEnctoken
 // @version      0.0.1
-// @description  The Script Save KiteEnctoken to jsonbin.com, which can be used to synchronized enctoken between local browser and Algo Trading Code at Remote Server
+// @description  The Script Save KiteEnctoken to jsonbin.io, which can be used to synchronized enctoken between local browser and Algo Trading Code at Remote Server
 // @author       Quantitative Algo Trading https://t.me/quantalgotradings
 // @match        https://kite.zerodha.com/*
 // @icon         https://www.google.com/s2/favicons?domain=zerodha.com
