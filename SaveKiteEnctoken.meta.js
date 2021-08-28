7e36a9d0847851d5a5f964483b22e8097f87d69f
Shabbir Hasan <68828793+ShabbirHasan1@users.noreply.github.com>
2021-08-28 11:43:25 +0530

    Update SaveKiteEnctoken.meta.js

--- SaveKiteEnctoken.meta.js
+++ SaveKiteEnctoken.meta.js
@@ -6,7 +6,16 @@
 // @author       Quantitative Algo Trading https://t.me/quantalgotradings
 // @match        https://kite.zerodha.com/*
 // @icon         https://www.google.com/s2/favicons?domain=zerodha.com
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @grant        GM_registerMenuCommand
+// @require      https://raw.githubusercontent.com/TechfaneTechnologies/SaveKiteEnctoken/main/betterCommon.js
+// @require      https://raw.githubusercontent.com/TechfaneTechnologies/SaveKiteEnctoken/main/monkeyconfig.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js
+// @require      https://raw.githubusercontent.com/kawanet/qs-lite/master/dist/qs-lite.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js
 // @downloadURL  https://github.com/TechfaneTechnologies/SaveKiteEnctoken/SaveKiteEnctoken.user.js
 // @updateURL    https://github.com/TechfaneTechnologies/SaveKiteEnctoken/SaveKiteEnctoken.meta.js
 // ==/UserScript==
